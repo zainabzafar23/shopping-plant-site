dccdac11461e1ce61abac97d8a9d9295f6cfa728
zainabzafar23 <zainabzafarkhan23@gmail.com>
2024-08-24 17:14:56 +0500

    commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-      return cart.reduce((total, item)=> total+ calculateTotalCost(item), 0);
+      return cart.reduce((total, item)=> total+ item.quantity, 0);
   };
 
   const handleContinueShopping = (e) => {
